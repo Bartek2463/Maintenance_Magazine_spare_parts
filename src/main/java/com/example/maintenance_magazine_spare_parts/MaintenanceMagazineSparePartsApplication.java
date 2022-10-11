3d8c950c3ec22c85961b6c7deee24e85e6d2c90d
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-11 22:59:30 +0200

    Initial commit

---
+++ src/main/java/com/example/maintenance_magazine_spare_parts/MaintenanceMagazineSparePartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenance_magazine_spare_parts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMagazineSparePartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMagazineSparePartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintenance_magazine_spare_parts/MaintenanceMagazineSparePartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenance_magazine_spare_parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMagazineSparePartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
