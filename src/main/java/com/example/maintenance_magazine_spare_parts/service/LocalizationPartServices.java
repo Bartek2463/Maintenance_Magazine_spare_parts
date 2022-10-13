56c27d341e8c5b40434c9cd70790636863ba3b0b
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-14 00:40:14 +0200

    refactor @Id eventLisner

--- src/main/java/com/example/maintenance_magazine_spare_parts/service/LocalizationPartServices.java
+++ src/main/java/com/example/maintenance_magazine_spare_parts/service/LocalizationPartServices.java
@@ -3,6 +3,8 @@ package com.example.maintenance_magazine_spare_parts.service;
 import com.example.maintenance_magazine_spare_parts.model.LocalizationPart;
 import com.example.maintenance_magazine_spare_parts.repository.LocalizationRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,27 +17,35 @@ public class LocalizationPartServices {
     private LocalizationRepository localizationRepository;
 
 
-
-    public List<LocalizationPart> getLocalizationParts(){
+    public List<LocalizationPart> getLocalizationParts() {
         return localizationRepository.findAll();
     }
 
 
-    public LocalizationPart getLocalizationById(Long id){
+    public LocalizationPart getLocalizationById(Long id) {
         return localizationRepository
                 .findById(id)
-                .orElseThrow(()->new NoSuchElementException("Not Exists Localization"));
+                .orElseThrow(() -> new NoSuchElementException("Not Exists Localization"));
     }
 
 
-    public LocalizationPart addLocalization(LocalizationPart localizationPart){
+    public LocalizationPart addLocalization(LocalizationPart localizationPart) {
         return localizationRepository.save(localizationPart);
     }
 
-    public void deleteLocalization(Long id){
+    public void deleteLocalization(Long id) {
         localizationRepository.deleteById(id);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void fillDBLocalization() {
+        addLocalization(new LocalizationPart(1l, "R4/3/3"));
+        addLocalization(new LocalizationPart(2l, "J2/1/6"));
+        addLocalization(new LocalizationPart(3l, "I4/2/2"));
+        addLocalization(new LocalizationPart(4l, "F2/1/3"));
+        addLocalization(new LocalizationPart(5l, "E2/3/5"));
+        addLocalization(new LocalizationPart(6l, "J3/1/6"));
+    }
 
 
 }
