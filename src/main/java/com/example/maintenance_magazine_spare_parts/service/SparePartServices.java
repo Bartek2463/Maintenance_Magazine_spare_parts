41bb058a901b45e1958b5500a72498a1c93a28ee
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-13 16:07:53 +0200

    create Test model data for controller and ride test for method.

---
+++ src/main/java/com/example/maintenance_magazine_spare_parts/service/SparePartServices.java
@@ -0,0 +1,26 @@
+package com.example.maintenance_magazine_spare_parts.service;
+
+import com.example.maintenance_magazine_spare_parts.model.SparePart;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+@Slf4j
+public class SparePartServices {
+
+    private AtomicLong idex = new AtomicLong(0);
+    private List<SparePart> spareParts = new LinkedList<>(){
+
+        {
+            add(new )
+        }
+    };
+
+
+
+
+}
