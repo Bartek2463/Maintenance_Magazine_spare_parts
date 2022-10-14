3e2609b98b341491a5f5660660cdb4ff69586d44
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-14 12:40:56 +0200

    add simple function, add notation @EwentLisner to add to H2 Database new object, for every one start application

--- src/main/java/com/example/maintenance_magazine_spare_parts/service/SparePartServices.java
+++ src/main/java/com/example/maintenance_magazine_spare_parts/service/SparePartServices.java
@@ -3,12 +3,10 @@ package com.example.maintenance_magazine_spare_parts.service;
 import com.example.maintenance_magazine_spare_parts.model.SparePart;
 import com.example.maintenance_magazine_spare_parts.repository.SparePartRepository;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.NoSuchElementException;
 
 @Service
 @AllArgsConstructor
@@ -17,11 +15,22 @@ public class SparePartServices {
     private SparePartRepository sparePartRepository;
 
 
-    public List<SparePart> getSpareParts(){
+    public List<SparePart> getSpareParts() {
         return sparePartRepository.findAll();
     }
 
+    public SparePart getSparePartById(Long id) {
+        return sparePartRepository
+                .findById(id)
+                .orElseThrow(() -> new NoSuchElementException("No exists Spare Part"));
+    }
 
+    public SparePart addSparePart(SparePart sparePart) {
+        return sparePartRepository.save(sparePart);
+    }
 
+    public void deleteSparePartById(Long id){
+        sparePartRepository.deleteById(id);
+    }
 
 }
