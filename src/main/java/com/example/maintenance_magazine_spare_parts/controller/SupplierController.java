9f341f28ca6fa5f35875dc789363646f8da3402a
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-17 00:42:53 +0200

    finish View for Supplier Parts

--- src/main/java/com/example/maintenance_magazine_spare_parts/controller/SupplierController.java
+++ src/main/java/com/example/maintenance_magazine_spare_parts/controller/SupplierController.java
@@ -5,7 +5,9 @@ import com.example.maintenance_magazine_spare_parts.service.SupplierPartServices
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
@@ -25,11 +27,6 @@ public class SupplierController {
         return "suppliers/suppliersList";
     }
 
-    @GetMapping("{id}")
-    public SupplierPart getSupplier(@PathVariable Long id) {
-        return supplierPartServices.getSupplierById(id);
-    }
-
     @GetMapping("/addSupplier")
     public String getAddSupplier() {
         return "suppliers/addNewSupplier";
@@ -42,9 +39,22 @@ public class SupplierController {
         return new RedirectView("/suppliers");
     }
 
-    @DeleteMapping
-    public void delateSupplierByid(@PathVariable Long id) {
+    @GetMapping("/editSupplier/{id}")
+    public String getEditSupplier(@PathVariable Long id,Model model) {
+        SupplierPart supplierById = supplierPartServices.getSupplierById(id);
+        model.addAttribute("supplier",supplierById);
+        return "suppliers/editSupplier";
+    }
+    @PostMapping("editSupplier/{id}")
+    public RedirectView postEditSupplier(@Valid SupplierPart supplierPart){
+        supplierPartServices.addSupplier(supplierPart);
+        return new RedirectView("/suppliers");
+    }
+
+    @PostMapping("/delete/{id}")
+    public RedirectView deleteSupplierByid(@PathVariable Long id) {
         supplierPartServices.deleteSupplierByid(id);
+        return new RedirectView("/suppliers");
     }
 
 
