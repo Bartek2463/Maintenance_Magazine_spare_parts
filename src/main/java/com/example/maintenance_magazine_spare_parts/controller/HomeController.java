fbab520ca533b7ebe9e9c09d7a85fd1db7a8631b
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-16 22:24:19 +0200

    update depedences for views and first endpoint

---
+++ src/main/java/com/example/maintenance_magazine_spare_parts/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.maintenance_magazine_spare_parts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "index";
+    }
+}
